66e4ebc777f4055c59fcd4b89e289f6ff4cbed66
s1n7ax <srineshnisala@gmail.com>
2022-08-22 15:06:05 +0530

    refactor: structural changes in lua snippets

--- lua/snips/lua/init.lua
+++ lua/snips/lua/init.lua
@@ -1,19 +1,18 @@
-local func = require('snips.lua.function')
-local function_with_doc = require('snips.lua.function_with_doc')
-local anonymous_func = require('snips.lua.anonymous_function')
-local import = require('snips.lua.import')
-local module = require('snips.lua.module')
-local variable = require('snips.lua.variable')
-local stdout = require('snips.lua.stdout')
+local module_util = require('snips.utils.module')
+local im = module_util.importer('snips.lua')
 
 local M = {
-  anonymous_func = anonymous_func,
-  func = func,
-  func_with_doc = function_with_doc,
-  import = import,
-  module = module,
-  stdout = stdout,
-  variable = variable,
+  anonymous_func = im('anonymous_function'),
+  import = im('import'),
+  stdout = im('stdout'),
+
+  ----------------------------------------------------------------------
+  --                              choice                              --
+  ----------------------------------------------------------------------
+  function_choice = im('function_choice'),
+  func_with_doc_choice = im('function_with_doc_choice'),
+  module_choice = im('module_choice'),
+  variable_choice = im('variable_choice'),
 }
 
 return M
