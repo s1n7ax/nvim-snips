a10bbaf7d2112da18977f78c332e0dccf86d01e0
s1n7ax <srineshnisala@gmail.com>
2023-02-04 23:33:02 +0530

    refactor: add ; at the end of stdout

--- lua/snips/java/primitives/stdout.lua
+++ lua/snips/java/primitives/stdout.lua
@@ -3,7 +3,7 @@ local fmt = require('luasnip.extras.fmt').fmt
 local i = ls.insert_node
 
 return function()
-  return fmt('System.out.println({})', {
+  return fmt('System.out.println({});', {
     i(1, 'value'),
   })
 end
