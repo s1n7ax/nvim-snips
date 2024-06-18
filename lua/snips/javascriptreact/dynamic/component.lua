1be62ef616a524bfe00c497768569a5239e03149
s1n7ax <srineshnisala@gmail.com>
2024-06-18 20:05:58 +0530

    feat: get react component name from the file name

--- lua/snips/javascriptreact/dynamic/component.lua
+++ lua/snips/javascriptreact/dynamic/component.lua
@@ -5,6 +5,7 @@ local fmt = require('luasnip.extras.fmt').fmt
 
 local i = ls.insert_node
 local f = ls.function_node
+local t = ls.text_node
 
 local ts_react = TSReact:new()
 
@@ -20,13 +21,14 @@ return function()
 				if not ts_react:has_react_import() then
 					vim.api.nvim_buf_set_lines(0, 0, 0, true, {
 						'import React from "react"',
+						'',
 					})
 				end
 
 				return {}
 			end, {}),
-			i(1, 'Name'),
-			i(2, ''),
+			t(vim.fn.expand('%:t:r')),
+			i(1, ''),
 		}
 	)
 end
