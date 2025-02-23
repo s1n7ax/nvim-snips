d94e64ee02cd4b1b67c2000425844b3a8b9f0a8d
s1n7ax <srineshnisala@gmail.com>
2025-02-24 01:04:17 +0530

    feat: try to guess the name for nextjs special files

--- lua/snips/typescriptreact/dynamic/component.lua
+++ lua/snips/typescriptreact/dynamic/component.lua
@@ -1,5 +1,4 @@
 local ls = require('luasnip')
--- local TSReact = require('ts-utils.typescriptreact')
 
 local fmt = require('luasnip.extras.fmt').fmt
 
@@ -7,15 +6,49 @@ local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
 
+local page_to_prefix_map = {
+	['page.tsx'] = 'Page',
+	['layout.tsx'] = 'Layout',
+	['error.tsx'] = 'ErrorPage',
+	['loading.tsx'] = 'LoadingPage',
+}
+
+local is_special_file = function(file_name)
+	for k in pairs(page_to_prefix_map) do
+		if k == file_name then
+			return true
+		end
+	end
+	return false
+end
+
 local curr_file = function()
+	local curr_file_with_ext = vim.fn.expand('%:t')
+
+	if is_special_file(curr_file_with_ext) then
+		local dir_name = vim.fn.expand('%:h:t')
+		return string.upper(dir_name:sub(1, 1))
+			.. dir_name:sub(2)
+			.. page_to_prefix_map[curr_file_with_ext]
+	end
+
+	-- use the current file name
 	return vim.fn.expand('%:t:r')
 end
--- local curr_file = f(function()
--- 	return vim.fn.expand('%:t:r')
--- end)
 
 return function()
 	return c(1, {
+		fmt(
+			[[
+			export default function {}() {{
+				return({})
+			}}
+			]],
+			{
+				f(curr_file),
+				i(1, ''),
+			}
+		),
 		fmt(
 			[[
 			export interface {}Props {{
